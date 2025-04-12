ea2cc8fba7afaa2e295c14b6a8ea5d47f1ab5f9d
thongheo <thongdoan742@gmail.com>
2025-04-12 19:32:40 +0700

    add Home UI

--- app/src/main/java/com/example/projectandroidapp_findingroom/pages/HomeScreen.kt
+++ app/src/main/java/com/example/projectandroidapp_findingroom/pages/HomeScreen.kt
@@ -1,30 +1,143 @@
 package com.example.projectandroidapp_findingroom.pages
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.projectandroidapp_findingroom.ContentScreen
 import com.example.projectandroidapp_findingroom.R
-
+import com.example.projectandroidapp_findingroom.ui.theme.fontFamily
 
 @Composable
 fun HomeScreen() {
-    Box(
+    Column(
         modifier = Modifier
-            .fillMaxSize()
-            .background(color = colorResource(R.color.main_color)),
-        contentAlignment = Alignment.Center
+            .fillMaxSize(),
     ){
-        Text(
-            "HomeScreen",
-            fontSize = 40.sp,
-            fontWeight = FontWeight.Bold
+        HeaderHomeScreen()
+        LazyColumn (modifier = Modifier.fillMaxHeight()){
+            items(30) { index ->
+                CardRoom()
+            }
+        }
+    }
+}
+
+@Composable
+fun HeaderHomeScreen() {
+    Surface(
+        color = colorResource(R.color.main_color),
+        modifier = Modifier
+            .height(140.dp)
+            .fillMaxWidth()
+    ) {
+        TextField(
+            value = "Tìm kiếm",
+            onValueChange = {},
+            textStyle = TextStyle(
+                fontFamily = fontFamily,
+                color = Color.Gray,
+                fontSize = 14.sp
+            ),
+            colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                focusedContainerColor = Color.White,
+                unfocusedContainerColor = Color.White,
+            ),
+            shape = RoundedCornerShape(14.dp),
+            modifier = Modifier
+                .padding(top = 60.dp, bottom = 20.dp, start = 20.dp, end = 20.dp)
         )
     }
+}
+
+@Preview (showBackground = true)
+@Composable
+fun CardRoom() {
+    val address = "597/14 Quang Trung, Phường 11, Gò Vấp, Hồ Chí Minh"
+    val price = "5 triệu"
+    val state = true
+        Surface(
+            shape = RoundedCornerShape(17.dp),
+            shadowElevation = 10.dp,
+            modifier = Modifier
+                .padding(start = 20.dp, end = 20.dp, top = 30.dp)
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.anh1),
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxWidth(),
+                    contentScale = ContentScale.Crop
+                )
+                Spacer(Modifier.padding(5.dp))
+                Column (
+                    modifier = Modifier
+                        .padding(10.dp)
+                ) {
+                    Text(
+                        "Địa chỉ: ${address}",
+                        fontFamily = fontFamily
+                    )
+                    Spacer(Modifier.padding(5.dp))
+                    Text(
+                        "Giá: ${price}",
+                        color = Color.Blue,
+                        fontFamily = fontFamily
+
+                    )
+                    Spacer(Modifier.padding(5.dp))
+                    Surface (
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        color = Color.LightGray,
+                        shape = RoundedCornerShape(17.dp)
+                    ) {
+                        Text(
+                            if (state) {
+                                "Còn trống 1 phòng"
+                            }else "Phòng đã có người cọc",
+                            modifier = Modifier
+                                .padding(10.dp)
+                                .fillMaxWidth(),
+                            color = Color.Black,
+                            textAlign = TextAlign.Center,
+                            fontFamily = fontFamily
+
+                        )
+                    }
+                }
+            }
+        }
 }
