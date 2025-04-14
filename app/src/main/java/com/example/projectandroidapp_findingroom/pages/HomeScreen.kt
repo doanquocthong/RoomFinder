467893dc4156c09ae19f3b0048c3ab3c1b57d1a3
thongheo <thongdoan742@gmail.com>
2025-04-14 18:05:34 +0700

    Thong commit

--- app/src/main/java/com/example/projectandroidapp_findingroom/pages/HomeScreen.kt
+++ app/src/main/java/com/example/projectandroidapp_findingroom/pages/HomeScreen.kt
@@ -1,9 +1,6 @@
 package com.example.projectandroidapp_findingroom.pages
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -11,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
@@ -21,18 +17,15 @@ import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.projectandroidapp_findingroom.ContentScreen
 import com.example.projectandroidapp_findingroom.R
 import com.example.projectandroidapp_findingroom.ui.theme.fontFamily
 
@@ -44,7 +37,7 @@ fun HomeScreen() {
     ){
         HeaderHomeScreen()
         LazyColumn (modifier = Modifier.fillMaxHeight()){
-            items(30) { index ->
+            items(30) {
                 CardRoom()
             }
         }
